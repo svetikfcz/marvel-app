47e1d06dbc2a7e8b73f07bb28c8470dc2903b3ca
Svetlana Efimova <svetlana.e08@gmail.com>
2021-10-18 21:19:25 +0300

    update char and charlist

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+import Spinner from '../spinner/Spinner';
+import img from './icons8-error-64.png';
+
+const ErrorMessage = () => {
+    return (
+        <img style={{ display: 'block', width: "64px", height: "64px",objectFit: 'contain', margin: "0 auto"}} src={img} alt="Error"/>  
+    )
+}
+
+export default ErrorMessage;
