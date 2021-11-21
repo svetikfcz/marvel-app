17d77c357f930adf7023acb7b5f036ed34f49bc4
Svetlana Efimova <svetlana.e08@gmail.com>
2021-11-21 20:42:34 +0300

    char search form

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,6 +4,7 @@ import CharInfo from "../charInfo/CharInfo";
 import CharList from "../charList/CharList";
 import RandomChar from "../randomChar/RandomChar";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import CharSearchForm from "../charSearchForm/CharSearchForm";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -18,17 +19,22 @@ const MainPage = () => {
     return (
         <>
             <ErrorBoundary>
-                <RandomChar />
+                <RandomChar/>
             </ErrorBoundary>
             <div className="char__content">
                 <ErrorBoundary>
-                    <CharList onCharSelected={onCharSelected} />
+                    <CharList onCharSelected={onCharSelected}/>
                 </ErrorBoundary>
-                <ErrorBoundary>
-                    <CharInfo charId={selectedChar} />
-                </ErrorBoundary>
+                <div>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharSearchForm/>
+                    </ErrorBoundary>
+                </div>
             </div>
-            <img className="bg-decoration" src={decoration} alt="vision" />
+            <img className="bg-decoration" src={decoration} alt="vision"/>
         </>
     )
 }
